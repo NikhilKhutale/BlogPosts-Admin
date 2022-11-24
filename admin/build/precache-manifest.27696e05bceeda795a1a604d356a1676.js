48c3b40a1eabfd4934c235de083298db07f43eff
Nikhil Khutale <nikhilkhutale500@gmail.com>
2022-11-24 16:49:05 +0530

    first commit

---
+++ admin/build/precache-manifest.27696e05bceeda795a1a604d356a1676.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1c4dc15ddcb914de343",
+    "url": "/static/js/main.76350d56.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "41989378daf9912a17d2",
+    "url": "/static/css/2.f879b5c6.chunk.css"
+  },
+  {
+    "revision": "41989378daf9912a17d2",
+    "url": "/static/js/2.ff682481.chunk.js"
+  },
+  {
+    "revision": "7ed2686d4dfd53a49890b2c7f80104b8",
+    "url": "/index.html"
+  }
+];
